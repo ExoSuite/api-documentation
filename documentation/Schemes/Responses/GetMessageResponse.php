abf9e65332a23d88f8a0941dbde1515686280dfd
Stan <stan.deneubourg@exosuite.fr>
2019-03-08 04:53:27 +0100

    Fix getMessages example response if OK

--- documentation/Schemes/Responses/GetMessageResponse.php
+++ documentation/Schemes/Responses/GetMessageResponse.php
@@ -9,7 +9,7 @@
 /**
  * @OA\Schema(
  *     schema="GetMessageResponse",
- *     required={"messages_array"}
+ *     required={"data"}
  *  )
  */
 class GetMessageResponse
@@ -17,28 +17,72 @@ class GetMessageResponse
     /**
      * @var array
      * @OA\Property(@OA\Items(type="message"),
-     *              example={"current_page => 1",
-    "data => array:30 {",
-    "0 => array:6 {",
-    "id => fc7ef8b0-414b-11e9-af5e-6fd5ac914d6a",
+     *              example={"id => fc7ef8b0-414b-11e9-af5e-6fd5ac914d6a",
     "contents => 7TdbHw75o4",
     "group_id => fc7c17b0-414b-11e9-b643-b3b9900c651c",
     "user_id => fc7a22f0-414b-11e9-9895-b19065e9260e",
     "created_at => 2019-03-08 02:43:42",
-    "updated_at => 2019-03-08 02:43:42",
-    "]",
-    "...other messages...",
-    "]",
-    "first_page_url => http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=1",
-    "from => 1",
-    "last_page => 2",
-    "last_page_url => http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=2",
-    "next_page_url => http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=2",
-    "path => http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message",
-    "per_page => 30",
-    "prev_page_url => null",
-    "to => 30",
-    "total => 40"})
+    "updated_at => 2019-03-08 02:43:42"})
      */
-    public $messages_array;
+    public $data;
+
+    /**
+     * @var string
+     * @OA\Property(example="http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=1")
+     */
+    public $first_page_url;
+
+    /**
+     * @var string
+     * @OA\Property(example="1")
+     */
+    public $from;
+
+    /**
+     * @var string
+     * @OA\Property(example="2")
+     */
+    public $last_page;
+
+    /**
+     * @var string
+     * @OA\Property(example="http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=2")
+     */
+    public $last_page_url;
+
+    /**
+     * @var string
+     * @OA\Property(example="http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message?page=2")
+     */
+    public $next_page_url;
+
+    /**
+     * @var string
+     * @OA\Property(example="http://api.exosuite.local/group/fc7c17b0-414b-11e9-b643-b3b9900c651c/message")
+     */
+    public $path;
+
+    /**
+     * @var string
+     * @OA\Property(example="30")
+     */
+    public $per_page;
+
+    /**
+     * @var string
+     * @OA\Property(example="null")
+     */
+    public $prev_page_url;
+
+    /**
+     * @var string
+     * @OA\Property(example="30")
+     */
+    public $to;
+
+    /**
+     * @var string
+     * @OA\Property(example="40")
+     */
+    public $total;
 }
