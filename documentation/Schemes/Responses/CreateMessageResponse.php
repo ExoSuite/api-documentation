4fa01d1515981921bbe2654b25dca07c9ff217cf
Loïc Lopez <loic.lopez@exosuite.fr>
2019-03-09 15:26:59 -0500

    add real format to uuid

--- documentation/Schemes/Responses/CreateMessageResponse.php
+++ documentation/Schemes/Responses/CreateMessageResponse.php
@@ -21,20 +21,17 @@ class CreateMessageResponse
     public $contents;
 
     /**
-     * @var string
-     * @OA\Property(example="67eeab70-19f4-11e9-b4d2-476c47235c92")
+     * @OA\Property(type="string", format="uuid")
      */
     public $user_id;
 
     /**
-     * @var string
-     * @OA\Property(example="67eeca70-19f4-11e9-b882-476c47252c92")
+     * @OA\Property(type="string", format="uuid")
      */
     public $group_id;
 
     /**
-     * @var string
-     * @OA\Property(example="67eeca70-19f4-1ae9-b122-469c47252c92")
+     * @OA\Property(type="string", format="uuid")
      */
     public $id;
 }
--- documentation/Schemes/Responses/GetGroupResponse.php
+++ documentation/Schemes/Responses/GetGroupResponse.php
@@ -21,14 +21,13 @@ class GetGroupResponse
     public $name;
 
     /**
-     * @var string
-     * @OA\Property()
+     * @OA\Property(type="string", format="uuid")
      */
     public $id;
 
     /**
      * @var array
-     * @OA\Property(@OA\Items(type="string"),
+     * @OA\Property(@OA\Items(type="string", format="uuid"),
      *             example={"67eeab70-19f4-11e9-b4d2-476c47235c92", "969fc1b0-1f28-11e9-9ac9-4da5046d4048"})
      */
     public $group_members;
